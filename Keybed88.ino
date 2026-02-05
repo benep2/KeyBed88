1b9c78f80f951687c52bdf14719136d826141971
Benedito Portela <132488559+benep2@users.noreply.github.com>
2026-02-05 15:08:00 +0000

    Add MIDI Serial Port Out
    Add MIDI seiral output port.

--- Keybed88.ino
+++ Keybed88.ino
@@ -1,21 +1,21 @@
 //################################################
 //#     Keybed Controller pi pico                #
 //#     By Benedito Portela - benep2@gmail.com   #
-//#            2025, Jan   Version: 1.00         #
+//#            2026, Fev   Version: 1.02         #
 //################################################
 //
 
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h>
 #include <MIDI.h>
-#define ROFF 9
-#define RVel press[90]
 // USB MIDI object
 Adafruit_USBD_MIDI usb_midi;
 // Create a new instance of the Arduino MIDI Library,
 // and attach usb_midi as the transport.
-   MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, Midi);
+MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, Midi);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MidiS);
 //Keys
+#define ROFF 9 //Read Pin Offset
 unsigned long long press[90];
 uint8_t rd,rdu,Key;
 uint8_t pressD[90];
@@ -24,10 +24,10 @@ uint8_t pedal=0,pread=0;
 unsigned long temp;
 #define PedalPin 21
 //Pitch Bend
-int bend,bendo,pith,pitho;
 #define OF 8
+int bend,bendo,pith,pitho;
 //Pots CC 
-uint8_t potCC[3]={21,22,23}; 
+uint8_t potCC[3]={07,91,93};// Volume, Reverb, Chorus 
 uint16_t pot[3],poto[3]={0,0,0},potr;
 
 void setup()  
@@ -39,11 +39,12 @@ void setup()
         pressD[i]=0;
         }
    //Name of device     
-   TinyUSBDevice.setManufacturerDescriptor("Benedito");
-   TinyUSBDevice.setProductDescriptor("KeyBed_88");
-   TinyUSBDevice.setSerialDescriptor("Keybed-001");
+  TinyUSBDevice.setManufacturerDescriptor("Benedito");
+  TinyUSBDevice.setProductDescriptor("KeyBed_88");
+  TinyUSBDevice.setSerialDescriptor("Keybed-001");
   Midi.begin(MIDI_CHANNEL_OMNI);
-  Serial.begin(115200);
+  MidiS.begin(MIDI_CHANNEL_OMNI);
+  Serial1.begin(31250);
   // Pins multiplex 4 bits 
   pinMode(5,OUTPUT);
   pinMode(6,OUTPUT);
@@ -63,7 +64,8 @@ void setup()
   pinMode(18,INPUT_PULLUP);
   pinMode(19,INPUT_PULLUP);
   pinMode(20,INPUT_PULLUP);
-  pinMode(PedalPin,INPUT_PULLUP); //Pedal pin
+  //Pedal Pin
+  pinMode(PedalPin,INPUT_PULLUP); 
 }
 
 void loop() // Main loop
@@ -76,29 +78,28 @@ void loop() // Main loop
       digitalWrite(5,(x & 8));// bit 3
       delayMicroseconds(10);
       for ( uint8_t y=0;y<6;y++) {
-        rd=digitalRead(y+ROFF); //ler contato low
-            Key=(x*6)+y; // Contato lido low
+        rd=digitalRead(y+ROFF); //Read low contact
+            Key=(x*6)+y; // Key definiton
             if ((rd==0) && (press[Key]==0)) {
-              press[Key]=micros();
-              digitalWrite(25,1);
+              press[Key]=micros(); //Read time
+              digitalWrite(25,1); // turned ON Led 
             } 
           if ((rd==1) && (press[Key]>0)) {
             press[Key]=0;
             NTOFF(Key+19);
             digitalWrite(25,0);
              }
-          rdu=digitalRead(y+ROFF+6); //ler contato up
+          rdu=digitalRead(y+ROFF+6); //Read up contact
             if ((rdu==0) &&(pressD[Key]==0))
               {
                pressD[Key]=1;
-               //CHATGPT
                 uint32_t dt = micros() - press[Key];
                 if (dt < 4500) dt = 4500;
                 if (dt > 100000) dt = 100000;
-                float x = (float)(dt - 4500) / (100000 - 4500);
-                x = sqrt(x);           // compressão de ação de martelo
-                float v = 1.0 - x;     // rápido = forte
-                v = pow(v, 1.3);       // ajuste fino (opcional)
+                float k = (float)(dt - 4500) / (100000 - 4500);
+                k = sqrt(k);           // Hammer aciton compress
+                float v = 1.0 - k;     // fast = strong
+                v = pow(v, 1.3);       // fine adjust (opcional)
                 uint8_t vel = (uint8_t)(v * 127.0);
                 if (vel < 1) vel = 1;
                 if (vel > 127) vel = 127;
@@ -109,9 +110,9 @@ void loop() // Main loop
                     
           }
 
-      }//fim for y
+      }//end for y
       
-  } //fim for x   
+  } //end for x   
 //Pedal
 if (millis()>=temp+5) {
 temp=millis();
@@ -124,8 +125,7 @@ if ((pread==1) && (pedal==0)){
      pedal=1;
      CC(64,127);
      }
-//Bend
-
+//Pitch Bend
 bend=analogRead(26);
    if ((bend>bendo+OF) || (bend<bendo-OF)) {
       bendo=bend;
@@ -143,45 +143,46 @@ bend=analogRead(26);
         pitho=pith;
         Pitch(pith);
       }
-      //Serial.println(bend);
-
-   } //end of bend
-//Potenciometers CC
+    } //end of bend
+//Potentiometers CC
   for (uint8_t c=0;c<3;c++){
      potr=analogRead(27+c);
       if ((potr>poto[c]+28) || (potr<poto[c]-28)) {
-     poto[c]=potr;
-     potr=map(potr,20,4076,0,128);
-     if (potr<0) potr=0;
-     if (potr>127) potr=127;
-     if (potr!=pot[c]) {
-      pot[c]=potr;
-      CC(potCC[c],potr);
-           }
+         poto[c]=potr;
+         potr=map(potr,20,4076,0,128);
+          if (potr<0) potr=0;
+          if (potr>127) potr=127;
+            if (potr!=pot[c]) {
+              pot[c]=potr;
+              CC(potCC[c],potr);
+              }
       }
   } //end of for CC
 
 }//end if pedal
-//Pitch Bend
-
 
 } //End Main loop 
-// Envio das mensagens para as duas portas
+// Send MIDI messengers in USB and MIDI Serial Port
 void NTON( unsigned char d1, unsigned char d2 )
    {
           Midi.sendNoteOn(d1,d2, 1);
+          MidiS.sendNoteOn(d1,d2, 1);
     }
 //Note Off    
 void NTOFF( unsigned char d1)
    {
           Midi.sendNoteOff(d1,0, 1);
+          MidiS.sendNoteOff(d1,0, 1);
     }
 //CC
 void CC( unsigned char d1, unsigned char d2 )
    {
           Midi.sendControlChange(d1,d2, 1);
+          MidiS.sendControlChange(d1,d2, 1);
     }
+    // Pitch Bend
 void Pitch( int d1 )
    {
           Midi.sendPitchBend(d1,1);
+          MidiS.sendPitchBend(d1,1);
     }
